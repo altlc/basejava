25da703ee034b84e7bb08506dde60a3f2077a6dc
altlc <altrealm@gmail.com>
2021-01-27 22:46:13 +0300

    Вступительный урок

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,62 @@
+import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int storageSize;
 
     void clear() {
+        for (int i = 0; i < storageSize; i++) {
+            storage[i] = null;
+        }
+        storageSize = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        int index = getIndexFromUuid(resume.toString());
+        if (index < storageSize) {
+            storage[index] = resume;
+        } else {
+            storage[storageSize] = resume;
+            storageSize++;
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        int index = getIndexFromUuid(uuid);
+        if (index < storageSize) {
+            return storage[index];
+        }
+        return new Resume();
     }
 
     void delete(String uuid) {
+        //int i;
+        for (int i = getIndexFromUuid(uuid); i < (storageSize - 1); i++) {
+            storage[i] = storage[i + 1];
+            storage[i + 1] = null;
+        }
+        storageSize--;
+    }
+
+    private int getIndexFromUuid(String uuid) {
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return i;
+            }
+        }
+        return 10000;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, storageSize);
     }
 
     int size() {
-        return 0;
+        return storageSize;
     }
 }
