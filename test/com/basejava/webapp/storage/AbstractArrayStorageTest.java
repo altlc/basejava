ad58e4fea9140a050ffeb7892d2fd8de4b4c5faa
altlc <altrealm@gmail.com>
2021-04-01 16:47:22 +0300

    Урок 5, ListStorage, Исправления

--- test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/basejava/webapp/storage/AbstractArrayStorageTest.java
@@ -1,8 +1,6 @@
 package com.basejava.webapp.storage;
 
-import static org.junit.Assert.*;
-
-public class AbstractArrayStorageTest extends AbstractStorageTest{
+public class AbstractArrayStorageTest extends AbstractStorageTest {
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage, AbstractArrayStorage.STORAGE_LIMIT);
     }
